ca6dd0fe4dec4db93d81f39d171648ab774a6457
Akshay123-java <akshaychidre1995@gmail.com>
2020-01-22 18:55:24 +0530

    Last time try push code in to new branch of branch name TestLast

--- src/com/hmm/TestDemo.java
+++ src/com/hmm/TestDemo.java
@@ -1,10 +1,16 @@
 package com.hmm;
 
 public class TestDemo {
+	
+	static String i="Akshay Chidre";
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Akshay hello");
+		
+		System.out.println(i);
 	}
 
 }
